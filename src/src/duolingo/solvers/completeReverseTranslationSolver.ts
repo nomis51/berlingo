45a9d92e80bf108848b17676bf70bffa3c5f93a1
nomis51 <nomis51@users.noreply.github.com>
2025-04-15 19:04:56 -0400

    FIX: #20 - challenge structure changed

--- src/src/duolingo/solvers/completeReverseTranslationSolver.ts
+++ src/src/duolingo/solvers/completeReverseTranslationSolver.ts
@@ -8,18 +8,31 @@ export class CompleteReverseTranslationSolver extends Solver {
 
     public solve(): Promise<void> {
         return new Promise<void>((resolve) => {
-            const inputs = Array.from(document.querySelectorAll("[data-test='challenge-text-input']"));
-
-            this.displayTokens.forEach(e => {
-                if (e.isBlank) return;
-
+            if (this.isToggledToTyping) {
+                const inputs = Array.from(document.querySelectorAll("[data-test='challenge-translate-input']"));
                 const input = inputs.shift() as HTMLInputElement;
-                ReactService.getReactFiber(input)
-                    ?.return
-                    ?.stateNode
-                    ?.props
-                    ?.onChange({target: {value: e.text}});
-            })
+                if (!input) return resolve();
+
+                const answer = this.displayTokens.map(e => e.text).join('');
+                this.insertTranslation(answer);
+            } else {
+                const inputs = Array.from(document.querySelectorAll("[data-test='challenge-text-input']"));
+                this.displayTokens.forEach(e => {
+                    if (!e.isBlank) return;
+
+                    const input = inputs.shift() as HTMLInputElement;
+                    if (!input) return;
+
+                    const fiber = ReactService.getReactFiber(input);
+                    if (!fiber) return;
+
+                    fiber?.return
+                        ?.stateNode
+                        ?.props
+                        ?.onChange({target: {value: e.text}});
+                });
+            }
+
             resolve();
         });
     }
